31926f16c89150c33e9bb6c8f9ed5c3b4c57cdd4
kecske5000 <marton.kristof@students.jedlik.eu>
2025-04-18 18:06:16 +0200

    Add project files.

---
+++ Kocsik/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kocsik
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string tulajdonsagNev)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(tulajdonsagNev));
+        }
+
+        private void search_BTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void new_BTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void modify_BTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void delete_BTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
